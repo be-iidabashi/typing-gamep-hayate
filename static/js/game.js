376551e76388729a7d5809e693ff22be6cced975
be-iidabashi <beengineer.iidabashi@gmail.com>
2026-01-04 15:35:56 +0900

    英単語タイピングゲーム完成

--- static/js/game.js
+++ static/js/game.js
@@ -2,12 +2,27 @@
 document.addEventListener("DOMContentLoaded",() => {
 
     //2
+    const timeText = document.getElementById("timeText");
+    let timeoutID;
     let startFlag = 0; // 0→開始前、１→開始待機、２→ゲーム中、３→終了
     let startTime;
     let missTypeCount = 0;
     let typeCount = 0;
     let current = 0;
     let letterCount= 0;
+    let typedText;
+    let untypedText;
+    const wordObjList = [];
+    const wordLength = 20;
+    const panelContainer = document.getElementsByClassName("panel-container")[0];
+    const wordCountText = document.getElementById("wordCount");
+    // wordLenghtに合わせて表示を動的に変更する。（cssを手動で変更する必要がある。）
+    document.getElementById("wordLength").textContent = `/${wordLength}`;
+    const missMountText = document.getElementById("missMount");
+    const infoBox = document.getElementById("info");
+    const scoreText = document.getElementById("score");
+    const otherResult = document.getElementById("other-result");
+    const resultSection = document.getElementById("results");
 
     //3
     const clearSound = document.getElementById("type_clear");
@@ -25,4 +40,212 @@ document.addEventListener("DOMContentLoaded",() => {
         };
         return array;
     };
+
+    function wordObjListMake(data){
+        //2
+        const lines = data.split("\n");
+        shuffleArray(lines);
+        //3
+        for(let i=0;i<wordLength;i++){
+            let word = lines[i].split(",");
+            wordObjList.push({
+                "untyped": word[0],
+                "typed": "",
+                "word": word[0],
+                "letterLength":word[0].length,
+            });
+        };
+    };
+
+    function displayTime() {
+        //1
+        const currentTime = Date.now() - startTime;
+        //2
+        const s = String(Math.floor(currentTime / 1000)).padStart(2, "0");
+        const ms = String(currentTime % 1000).padStart(3, "0");
+        timeText.textContent = `${s}.${ms}`;
+        //3
+        timeoutID = setTimeout(displayTime, 10);
+    }
+
+    function createPanels() {
+        // 1
+        panelContainer.innerHTML = "";
+        for (let i = 0; i < wordLength ; i++) {
+            //2
+            const panel = document.createElement("div");
+            const typedSpan = document.createElement("span");
+            const untypedSpan = document.createElement("span");
+
+            panel.id = "panel-" + i;
+            panel.className = "panel";
+            typedSpan.id = "typed-"+i;
+            typedSpan.className = "typed";
+            untypedSpan.id = "untyped-"+i;
+            untypedSpan.className = "untyped"
+
+            //3
+            untypedSpan.textContent = wordObjList[i]["untyped"];
+            //4
+            letterCount += wordObjList[i]["letterLength"];
+
+            panel.appendChild(typedSpan);
+            panel.appendChild(untypedSpan);
+            panelContainer.appendChild(panel);
+        }
+        //5
+        panelContainer.classList.add("panel-container-play");
+        document.getElementById("panel-0").classList.add("active");
+    }
+
+    function highlightCurrentPanel() {
+        //1
+        let currentPanel = document.getElementById(`panel-${current - 1}`);
+        let nextPanel = document.getElementById(`panel-${current}`);
+        //2
+        currentPanel.classList.remove("active");
+        currentPanel.classList.add("faded");
+        nextPanel.classList.add("active");
+    };
+
+    function processStartGame (){
+        //1
+        for (let i = 3, j = 0; i >= 1; i--, j++) {
+            setTimeout(() => {
+                infoBox.textContent = i;
+                countSound.currentTime = 0;
+                countSound.play();
+            }, j*1000);
+        };
+        //2
+        setTimeout(async ()=> {
+            startFlag = 2;
+            infoBox.textContent = "";
+            await fetch(`csv/word-${level}.csv`)
+                .then(response => response.text())
+                .then(data => wordObjListMake(data));
+            createPanels();
+            startSound.currentTime = 0;
+            startSound.play();
+            startTime = Date.now();
+            displayTime();
+            typedText = document.getElementById(`typed-${current}`);
+            untypedText = document.getElementById(`untyped-${current}`);
+        },3000);
+    }
+
+    function inputCheck(key){
+        // Userの入力数カウント用の変数をインクリメントする。
+        typeCount += 1;
+
+        // 正解のキーをタイプしたら
+        //1(a)
+        if(key == wordObjList[current]["untyped"].charAt(0)){
+            // 音声再生ごとに、最初から流れるようにする。
+            clearSound.currentTime = 0;
+            clearSound.play();
+
+            //1(b)
+            wordObjList[current]["typed"] = wordObjList[current]["typed"] + wordObjList[current]["untyped"].charAt(0);
+            wordObjList[current]["untyped"] = wordObjList[current]["untyped"].substring(1);
+
+            //1(c)
+            typedText.textContent = wordObjList[current]["typed"]
+            untypedText.textContent = wordObjList[current]["untyped"]
+
+            // ラスト1文字→次のワードへ
+            //2
+            if(wordObjList[current]["untyped"].length == 0){
+
+                //1単語分終了したので、current インデックスをインクリメントする。
+                current += 1;
+                wordCountText.textContent = current;
+
+                // ゲームの最終単語→ゲーム終了
+                //3(a)
+                if(current == wordLength){
+                    processEndGame();
+                }
+                //3(b)
+                else{
+                    highlightCurrentPanel();
+                    //3(c)
+                    typedText = document.getElementById(`typed-${current}`)
+                    untypedText = document.getElementById(`untyped-${current}`)
+                }
+            }
+        }
+        //4
+        else{
+            missSound.currentTime = 0;
+            missSound.play();
+            missTypeCount += 1;
+            missMountText.textContent = missTypeCount;
+        }
+    }
+
+    const levelBtns = document.querySelectorAll(".level_btn");
+    //active-leveクラスがついたlabelタグの子要素のinputタグを取得する。
+    let radioInput = document.querySelector(".active-level input");
+    let level = radioInput.value;
+
+    function handleLevelChange(newRadioInput){
+        //今まで選択していたradioボタンと異なれば
+        //1
+        if(radioInput !== newRadioInput){
+            level = newRadioInput.value;
+            newRadioInput.parentElement.classList.add("active-level");
+            radioInput.parentElement.classList.remove("active-level");
+            radioInput = newRadioInput;
+        };
+
+    }
+
+    //2
+    levelBtns.forEach(element => {
+        element.querySelector("input").addEventListener("click",(event) => {
+            handleLevelChange(event.target);
+        });
+    });
+
+    //1
+    window.addEventListener("keydown", (event) => {
+        //2
+        if(startFlag == 0 && event.key == " "){
+            startFlag = 1
+            processStartGame();
+        }
+        //3
+        else if(startFlag == 2 && event.key.length == 1 && event.key.match(/^[a-zA-Z0-9!-/:-@\[-`{-~\s]*$/)){
+            inputCheck(event.key);
+        }
+        else if(startFlag == 3 && (event.key =="Enter" || event.key == "Escape")){
+            this.location.reload();
+        };
+    })
+
+    function processEndGame(){
+        //1
+        clearTimeout(timeoutID);
+        const stopTime = (Date.now() - startTime);
+        //2
+        const score = parseInt((typeCount / stopTime) * 60000 * (letterCount / typeCount) ** 3);
+        scoreText.textContent = `SCORE : ${score}`;
+        otherResult.textContent = `合計入力文字数（ミスを含む):${typeCount}`;
+        resultSection.style.display = "flex";
+        //3
+        // 全パネルのハイライトを消す
+        for (let i = 0; i < wordLength; i++) {
+            const panel = document.getElementById("panel-" + i);
+            panel.classList.remove("active","faded");
+        };
+        //4
+        startFlag = 3;
+        window.scrollTo({
+            top: 100,      // 縦スクロールの位置
+            left: 0,     // 横スクロールの位置（通常は 0 のままでOK）
+            behavior: "smooth"
+        });
+    };
+
 });
